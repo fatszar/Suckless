5b46b1a1a4c470633b3781c3b2cadd1670326e2a
Tadeusz Szar <tadeusz.szar@gmail.com>
2023-03-13 17:03:31 +0100

    dwmblocks-async added

---
+++ dwmblocks-async/config.h
@@ -0,0 +1,14 @@
+#define CMDLENGTH 45
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+//	BLOCK("sb-mail",    1800, 17),
+//	BLOCK("sb-music",   0,    18),
+//	BLOCK("sb-disk",    1800, 19),
+//	BLOCK("sb-loadavg", 5,    21),
+//	BLOCK("sb-mic",     0,    26),
+//	BLOCK("sb-record",  0,    27),
+//	BLOCK("volume", 0,    5),
+    	BLOCK("date",   1800, 1)
+};
